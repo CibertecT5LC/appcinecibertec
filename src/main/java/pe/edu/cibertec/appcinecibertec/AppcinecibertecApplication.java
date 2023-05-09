f032ef979eb6a6c9720785fe0ced9d366c15ceaf
CibertecT5LC <Cibertec_T5LC@outlook.es>
2023-05-08 19:40:36 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/AppcinecibertecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appcinecibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcinecibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcinecibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appcinecibertec/AppcinecibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appcinecibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcinecibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
