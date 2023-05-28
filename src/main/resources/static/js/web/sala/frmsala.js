91ffd88303e1246e425507e8a1a25dcff6a43b9a
CibertecT5LC <Cibertec_t5lc@outlook.es>
2023-05-28 10:13:31 -0500

    funciones de jquery.

--- src/main/resources/static/js/web/sala/frmsala.js
+++ src/main/resources/static/js/web/sala/frmsala.js
@@ -1,6 +1,7 @@
 $(document).on("click", "#btnagregar", function(){
 	$("#txtdescripcion").val("");
 	$("#txtnroasientos").val("");
+	$("#hddidregistrosala").val("0");
 	$("#cboestado").empty();
 	$.ajax({
 		type: "GET",
@@ -15,6 +16,32 @@ $(document).on("click", "#btnagregar", function(){
 								${value.descestado}</option>`
 							);
 				})
+			}			
+		}
+	})
+	$("#modalSala").modal("show");
+});
+
+$(document).on("click", ".btnactualizar", function(){
+	$("#txtdescripcion").val($(this).attr("data-descsala"));
+	$("#txtnroasientos").val($(this).attr("data-asientos"));
+	$("#hddidregistrosala").val($(this).attr("data-idsala"));
+	$("#cboestado").empty();
+	var idestado = $(this).attr("data-idestado");
+	$.ajax({
+		type: "GET",
+		url: "/Estado/listarEstados",
+		dataType: "json",
+		success: function(resultado){
+			//console.log(resultado);
+			if(resultado.length > 0){
+				$.each(resultado, function(index, value){
+					$("#cboestado").append(
+							`<option value="${value.idestado}">
+								${value.descestado}</option>`
+							);
+				})
+				$("#cboestado").val(idestado);
 			}			
 		}
 	})
